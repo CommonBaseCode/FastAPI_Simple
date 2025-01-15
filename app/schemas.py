0606210cf2901da2b80b5fe3f6e9de3a16e823b3
minu-riemann <riemann9110@gmail.com>
2025-01-16 08:28:46 +0900

    [Fix] Modifying Config to ConfigDict to Fit Pydantic 2.x

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class UserBase(BaseModel):
     username: str
@@ -10,5 +10,4 @@ class UserCreate(UserBase):
 class UserResponse(UserBase):
     id: int
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
