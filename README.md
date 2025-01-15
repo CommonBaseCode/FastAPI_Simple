ae91a94a111d0b10d719d8a33d1043182afaf64a
minu-riemann <riemann9110@gmail.com>
2025-01-15 15:33:40 +0900

    Base Code Generations

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# FastAPI_Simple
+### 실행
++ uvicorn app.main:app --reload
+
+### 참조명령어
++ pip install -r requirements.txt
++ pip freeze > requirements.txt
++ pip install pytest
++ pytest
+
+### API 테스트트
++ http://127.0.0.1:8000/로 접속
++ Swagger 문서: http://127.0.0.1:8000/docs
++ Redoc 문서: http://127.0.0.1:8000/redoc
---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from app.routes import router
+from app.database import Base, engine
+import uvicorn
+
+# Creating a Database Table
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Router Registration
+app.include_router(router, prefix="/users", tags=["Users"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the REST API!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
